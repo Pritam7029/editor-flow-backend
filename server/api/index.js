f13164d1cd2e17c9b553dc571ea2c0f390ecb176
Pritam7029 <pritamchakraborty7029@gmail.com>
2026-06-15 01:32:19 +0530

    error checking

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,22 @@
-const app = require('../app');
+let app;
+let initError;
 
-module.exports = app;
+try {
+    app = require('../app');
+} catch (err) {
+    console.error('Failed to initialize app:', err.message);
+    console.error(err.stack);
+    initError = err;
+}
+
+module.exports = (req, res) => {
+    if (initError) {
+        res.status(500).json({
+            error: 'App failed to initialize',
+            message: initError.message,
+            stack: initError.stack
+        });
+        return;
+    }
+    return app(req, res);
+};
